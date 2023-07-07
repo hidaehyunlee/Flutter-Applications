85742304813f35326c7dec13afe5261c143ab557
Daehyun Lee <leehi0210@gmail.com>
2023-07-07 13:47:12 +0900

    call google book api, parse json

--- watcha_pedia/lib/book_service.dart
+++ watcha_pedia/lib/book_service.dart
@@ -1,7 +1,29 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 import 'book.dart';
 
 class BookService extends ChangeNotifier {
   List<Book> bookList = []; // 책 목록
+
+  void search(String q) async { // await 를 쓰려면 함수명 뒤에 async를 써야함
+    if (q.isNotEmpty) {
+      Response res = await Dio().get( // 동기로 (get은 원래 비동기)
+        "https://www.googleapis.com/books/v1/volumes?q=$q&startIndex=0&maxResults=40",
+      );      
+      
+      List items = res.data["items"];
+      for (Map<String, dynamic> item in items) {
+        Book book = Book(
+          id: item['id'],
+          title: item['volumeInfo']['title'] ?? "",
+          subtitle: item['volumeInfo']['subtitle'] ?? "",
+          thumbnail: item['volumeInfo']['imageLinks']?['thumbnail'] ??
+              "https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg",
+          previewLink: item['volumeInfo']['previewLink'] ?? "",
+        );
+        bookList.add(book);
+      }
+    }
+  }
 }
