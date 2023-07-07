a45211d13671d2412a6387163903b425d6466518
Daehyun Lee <leehi0210@gmail.com>
2023-07-07 14:00:03 +0900

    add ListTile(leading, title, subtitle, tariling)

--- watcha_pedia/lib/book_service.dart
+++ watcha_pedia/lib/book_service.dart
@@ -6,12 +6,16 @@ import 'book.dart';
 class BookService extends ChangeNotifier {
   List<Book> bookList = []; // 책 목록
 
-  void search(String q) async { // await 를 쓰려면 함수명 뒤에 async를 써야함
+  void search(String q) async {
+    // await 를 쓰려면 함수명 뒤에 async를 써야함
+    bookList.clear(); // 검색 버튼 누를때 이전 데이터들을 지워주기
+
     if (q.isNotEmpty) {
-      Response res = await Dio().get( // 동기로 (get은 원래 비동기)
+      Response res = await Dio().get(
+        // 동기로 (get은 원래 비동기)
         "https://www.googleapis.com/books/v1/volumes?q=$q&startIndex=0&maxResults=40",
-      );      
-      
+      );
+
       List items = res.data["items"];
       for (Map<String, dynamic> item in items) {
         Book book = Book(
@@ -25,5 +29,6 @@ class BookService extends ChangeNotifier {
         bookList.add(book);
       }
     }
+    notifyListeners(); // 해당 Service 의 Consumer 로 등록된 모든 위젯의 builder 함수를 재호출해 화면을 새로고침
   }
 }
